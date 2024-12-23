f85d8368a05da2a9a1111c9d0f551ef0a3ef2e02
arunstarportal <arun@starportal.co>
2024-12-23 17:47:16 +0530

    change backend url

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://tonbets.fun:6900"; // dev
-export const BASE_URL = "http://localhost:6900"; // local
+export const BASE_URL = "https://tonbets.fun:6900"; // dev
+// export const BASE_URL = "http://localhost:6900"; // local
 // export const BASE_URL = "https://starportal.space:6901"; // prod
